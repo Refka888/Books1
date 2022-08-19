0e1389dfdb1d47616f82fdaaa3e10a1ff7ba01c4
Gmati <refka.gmati@gmail.com>
2022-08-19 11:19:28 +0100

    JMSSerializer

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -5,8 +5,20 @@ namespace App\Entity;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\BookRepository;
-use Symfony\Component\Serializer\Annotation\Groups;
+use JMS\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @Hateoas\Relation(
+ *      "self",
+ *      href = @Hateoas\Route(
+ *          "detailBook",
+ *          parameters = { "id" = "expr(object.getId())" }
+ *      ),
+ *      exclusion = @Hateoas\Exclusion(groups="getBooks")
+ * )
+ *
+ */
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 class Book
